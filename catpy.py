492c7ea97b4e0f07b09633f6a02c25071eb4a77c
soonsimple <sun_chengbo@126.com>
2025-08-04 14:26:16 +0800

    modify help info

--- catpy.py
+++ catpy.py
@@ -7,10 +7,10 @@ catpy - A Python version of the classic Unix command 'cat'.
 Usage: catpy [options] [file ...]
 
 Options:
-  -n, --number      Number the output lines.
-  -E, --ending      Display a '$' at the end of each line.
-  -h, --help        Show this help message and exit.
-  -v, --version     Show the version of catpy and exit.
+  -n, --number  -   Number the output lines.
+  -E, --ending  -   Display a '$' at the end of each line.
+  -h, --help    -   Show this help message and exit.
+  -v, --version -   Show the version of catpy and exit.
   
 If no files are specified, catpy will read from standard input, 
 and write to standard output.
